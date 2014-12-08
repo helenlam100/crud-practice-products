529389d234c0efcb93c5744241871140c45669d3
Helen Lam <helen.f.lam100@gmail.com>
2014-12-08 13:02:24 -0800

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  root to: 'products#index'
+  resources :products
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
